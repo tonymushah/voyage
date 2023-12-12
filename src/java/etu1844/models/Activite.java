95ee485199a169c3928d24aca81a16cde632fba5
tonymushah <tonyramanantsalama@gmail.com>
2023-12-12 16:32:24 +0300

    Added Activite

---
+++ src/java/etu1844/models/Activite.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package etu1844.models;
+
+/**
+ *
+ * @author tonymushah
+ */
+public class Activite {
+    
+}
