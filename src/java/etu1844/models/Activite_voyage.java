5993a4c2b312335562f9c907e0f851a25fd34f85
Andraina_Harena <andrainaramaro@gmail.com>
2023-12-19 13:40:16 +0300

    feat added Activite_voyage class

---
+++ src/java/etu1844/models/Activite_voyage.java
@@ -0,0 +1,59 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package etu1844.models;
+
+/**
+ *
+ * @author Andra
+ */
+public class Activite_voyage {
+    private int id;
+    private int voyage;
+    private int activite;
+    private int duree;
+    private int nombre;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getVoyage() {
+        return voyage;
+    }
+
+    public void setVoyage(int voyage) {
+        this.voyage = voyage;
+    }
+
+    public int getActivite() {
+        return activite;
+    }
+
+    public void setActivite(int activite) {
+        this.activite = activite;
+    }
+
+    public int getDuree() {
+        return duree;
+    }
+
+    public void setDuree(int duree) {
+        this.duree = duree;
+    }
+
+    public int getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(int nombre) {
+        this.nombre = nombre;
+    }
+    
+    
+}
