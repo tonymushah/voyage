89576a94f14898cb1f6c1d4a9ec320170133f20d
tonymushah <tonyramanantsalama@gmail.com>
2024-01-11 13:54:47 +0300

    added localite insert

--- src/java/etu1844/models/Localite.java
+++ src/java/etu1844/models/Localite.java
@@ -4,7 +4,10 @@
  */
 package etu1844.models;
 
+import etu1844.utils.OffsetLimit;
+import java.sql.ResultSet;
 import java.sql.Statement;
+import java.util.Optional;
 import mg.tonymushah.dbconnection.DBConnect;
 import mg.tonymushah.dbconnection.utils.annotations.Column;
 import mg.tonymushah.dbconnection.utils.annotations.PrimaryKey;
@@ -44,4 +47,39 @@ public class Localite {
         Statement stmt = con.createStatement();
         stmt.execute(String.format("INSERT INTO localite(nom) values ('%s')", this.getNom()));
     }
+    
+    public static Localite[] getLocalites(DBConnect con, Optional<OffsetLimit> ol) throws Exception {
+        if (ol.isEmpty() || ol == null) {
+            Statement stmt = con.getConnection().createStatement();
+
+            String req = String.format("SELECT id, nom FROM localite");
+            ResultSet res = stmt.executeQuery(req);
+            return con.resultset_toObjects(res, Localite.class);
+        } else {
+            OffsetLimit ol_ = ol.get();
+            Statement stmt = con.getConnection().createStatement();
+
+            String req = String.format("SELECT id, nom FROM localite LIMIT %d OFFSET %d", ol_.limit(), ol_.offset());
+            ResultSet res = stmt.executeQuery(req);
+            return con.resultset_toObjects(res, Localite.class);
+        }
+
+    }
+
+    public Localite(int id, String nom) {
+        this.setId(id);
+        this.setNom(nom);
+    }
+
+    public Localite() {
+    }
+
+    public Localite(int id) {
+        this.setId(id);
+    }
+
+    public Localite(String nom) {
+        this.setNom(nom);
+    }
+    
 }
--- src/java/etu1844/servlets/localite/LocaliteInsert.java
+++ src/java/etu1844/servlets/localite/LocaliteInsert.java
@@ -4,30 +4,52 @@
  */
 package etu1844.servlets.localite;
 
+import etu1844.models.Localite;
+import etu1844.servlets.PostGresInitServlet;
 import java.io.IOException;
 import java.io.PrintWriter;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import mg.tonymushah.dbconnection.DBConnect;
+import mg.tonymushah.dbconnection.PostGresDefault;
 
 /**
  *
  * @author Andra
  */
 @WebServlet(name = "Localite-insert", urlPatterns = {"/Localite/insert"})
-public class Localite extends HttpServlet {
+public class LocaliteInsert extends PostGresInitServlet {
+
+    protected Localite insert(HttpServletRequest request) throws Exception {
+        Localite local = new Localite(request.getParameter("nom"));
+        DBConnect con = new PostGresDefault();
+        con.connect();
+        try {
+            local.insert(con);
+            con.getConnection().commit();
+        } catch (Exception ex) {
+            con.getConnection().rollback();
+        } finally {
+            con.close();
+        }
+        return local;
+    }
+
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        String nom=request.getParameter("nom");
-        
-        response.setContentType("text/html");
+        try {
+            Localite local = this.insert(request);
+            response.setContentType("text/html");
 
-        PrintWriter out = response.getWriter();
-        out.println("<h1>RESULTAT</h1>");
-        out.println("Nom:"+nom);
+            PrintWriter out = response.getWriter();
+            out.println("<h1>RESULTAT</h1>");
+            out.println("<p>Nom: " + local.getNom() + "</p>");
+        } catch (Exception ex) {
+            this.handle_execption(response, ex);
+        }
     }
-  
+
 }
